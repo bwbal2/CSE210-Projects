423f66a8f42c38d2131e9d287fa94f606a644d8f
bwbal2 <94023968+bwbal2@users.noreply.github.com>
2023-05-16 17:50:05 -0600

    Learning03, fractions

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,35 @@
+using System;
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+
+    }
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+
+    }
+
+    public void printFraction()
+    {
+        Console.WriteLine($"{_top}/{_bottom}");
+    }
+    public void printDecimal()
+    {
+        Console.WriteLine($"{(float)_top/(float)_bottom}");
+    }
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac1 = new Fraction();
+        Fraction frac2 = new Fraction(2);
+        Fraction frac3 = new Fraction(3,4);
+        Fraction frac4 = new Fraction(5,6);
+        List<Fraction> fractions = new List<Fraction>{frac1,frac2,frac3,frac4};
+        foreach (Fraction f in fractions)
+        {
+            f.printFraction();
+            f.printDecimal();
+        }
+
     }
 }
